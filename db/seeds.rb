c831b571d259cb60f72ee4404812fc2e9b9d9c6d
rztprog <rztprog@outlook.fr>
2026-04-08 18:18:15 +0200

    Add seed sample

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Ops3dSetting.find_or_create_by!(id: 1) do |settings|
+  settings.shipping_mode = "flat_rate"
+  settings.shipping_price_cents = 490
+end
+
+admin = User.find_or_create_by!(email: "admin@ops3d.local") do |user|
+  user.first_name = "Admin"
+  user.last_name = "Ops3d"
+  user.admin = true
+end
+
+cat1 = Category.find_or_create_by!(name: "Fixations")
+cat2 = Category.find_or_create_by!(name: "Supports")
+
+Product.find_or_create_by!(name: "Support caméra") do |product|
+  product.description = "Support imprimé 3D robuste"
+  product.price_cents = 2490
+  product.category = cat2
+  product.published = true
+  product.available = true
+end
