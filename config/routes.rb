946b40ea64aa4f6f11b4b185df5975e5a3f5b241
rztprog <rztprog@outlook.fr>
2026-04-12 17:32:47 +0200

    Add cart_items,carts,orders,products controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get "orders/new"
+  get "orders/create"
+  get "orders/show"
+  get "orders/index"
+  get "cart_items/create"
+  get "cart_items/update"
+  get "cart_items/destroy"
+  get "carts/show"
+  get "products/index"
+  get "products/show"
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
