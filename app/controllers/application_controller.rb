fe4cfcefb118f83b4f71538a9cf65a0acf4eec39
rztprog <rztprog@outlook.fr>
2026-04-13 10:41:39 +0200

    Add cart controller/view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,14 @@ class ApplicationController < ActionController::Base
   # Guest_token pour le panier
   before_action :set_guest_token
 
+  def current_cart
+    if user_signed_in?
+      current_user.cart || current_user.create_cart
+    else
+      Cart.find_or_create_by!(guest_token: session[:guest_token])
+    end
+  end
+
   def set_guest_token
     session[:guest_token] ||= SecureRandom.uuid
   end
