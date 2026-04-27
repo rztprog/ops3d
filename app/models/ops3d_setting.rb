52ce10765a85d7a1dcae65a444e347425c8dec93
rztprog <rztprog@outlook.fr>
2026-04-27 18:28:53 +0200

    Add validations to ops3dsetting model

--- app/models/ops3d_setting.rb
+++ app/models/ops3d_setting.rb
@@ -3,4 +3,19 @@ class Ops3dSetting < ApplicationRecord
 
   validates :shipping_mode, presence: true, inclusion: { in: SHIPPING_MODES }
   validates :shipping_price_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  validate :flat_rate_requires_positive_price
+
+  def shipping_price_euros
+    shipping_price_cents.to_f / 100
+  end
+
+  private
+
+  def flat_rate_requires_positive_price
+    return unless shipping_mode == "flat_rate"
+    return if shipping_price_cents.to_i.positive?
+
+    errors.add(:shipping_price_cents, "doit être supérieur à 0 pour une livraison forfaitaire")
+  end
 end
