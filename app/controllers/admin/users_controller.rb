4e173e4572e4361619f9fe5ee644974e286b0ba6
rztprog <rztprog@outlook.fr>
2026-04-28 23:32:55 +0200

    Add admin user index and show, add views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,19 @@
-class Admin::UsersController < ApplicationController
-  def index
-  end
+module Admin
+  class UsersController < BaseController
+    before_action :set_user, only: [ :show ]
 
-  def show
+    def index
+      @users = User.includes(:orders).order(created_at: :desc)
+    end
+
+    def show
+      @orders = @user.orders.order(created_at: :desc)
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
   end
 end
