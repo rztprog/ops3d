8554f1b12eb2602f74fb16bc5c1ed7523ede838a
rztprog <rztprog@outlook.fr>
2026-05-01 00:53:49 +0200

    Add mobile navbar to admin

--- app/javascript/controllers/admin_navbar_controller.js
+++ app/javascript/controllers/admin_navbar_controller.js
@@ -1,11 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["menu", "openIcon", "closeIcon"]
+  static targets = ["menu"]
 
   toggle() {
     this.menuTarget.classList.toggle("hidden")
-    this.openIconTarget.classList.toggle("hidden")
-    this.closeIconTarget.classList.toggle("hidden")
+  }
+
+  close() {
+    this.menuTarget.classList.add("hidden")
   }
 }
