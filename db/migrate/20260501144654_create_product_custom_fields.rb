12166832bea38c23f3dc1a005913afd69e4e4583
rztprog <rztprog@outlook.fr>
2026-05-01 16:50:10 +0200

    Fix createproductcustomfields migration

--- db/migrate/20260501144654_create_product_custom_fields.rb
+++ db/migrate/20260501144654_create_product_custom_fields.rb
@@ -2,10 +2,10 @@ class CreateProductCustomFields < ActiveRecord::Migration[8.1]
   def change
     create_table :product_custom_fields do |t|
       t.references :product, null: false, foreign_key: true
-      t.string :label
-      t.string :field_type
-      t.boolean :required
-      t.integer :position
+      t.string :label, null: false
+      t.string :field_type, null: false, default: "text"
+      t.boolean :required, null: false, default: false
+      t.integer :position, null: false, default: 0
       t.string :placeholder
 
       t.timestamps
