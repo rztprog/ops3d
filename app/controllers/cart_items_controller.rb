283c762c80e525196d2607fb5a1884cc45f7caf7
rztprog <rztprog@outlook.fr>
2026-05-01 18:12:50 +0200

    Fix cart do not merge with current cart if he has custom fields

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,17 +1,43 @@
 class CartItemsController < ApplicationController
   def create
+    @product = Product.find(params[:product_id])
     @cart = ensure_cart
-    product = Product.find(params[:product_id])
 
-    cart_item = @cart.cart_items.find_by(product: product)
+    custom_fields = @product.product_custom_fields.ordered
+    custom_values = params[:custom_fields] || {}
 
-    if cart_item
-      cart_item.increment!(:quantity)
-    else
-      @cart.cart_items.create!(product: product, quantity: 1)
+    missing_required_fields = custom_fields.select do |field|
+      field.required? && custom_values[field.id.to_s].blank?
     end
 
-    redirect_to cart_path
+    if missing_required_fields.any?
+      redirect_to product_path(@product), alert: "Merci de renseigner tous les champs obligatoires."
+      return
+    end
+
+    if custom_fields.any?
+      cart_item = @cart.cart_items.create!(
+        product: @product,
+        quantity: 1
+      )
+
+      custom_fields.each do |field|
+        value = custom_values[field.id.to_s]
+
+        next if value.blank? && !field.required?
+
+        cart_item.cart_item_custom_field_values.create!(
+          product_custom_field: field,
+          value: value
+        )
+      end
+    else
+      cart_item = @cart.cart_items.find_or_initialize_by(product: @product)
+      cart_item.quantity += 1
+      cart_item.save!
+    end
+
+    redirect_to cart_path, notice: "Produit ajouté au panier."
   end
 
   def update
