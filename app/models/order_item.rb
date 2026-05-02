fc5b07b739dff1eb82d4bcb8c30ccaa1806bc047
rztprog <rztprog@outlook.fr>
2026-05-02 13:29:21 +0200

    Add custom fields in orders/show and admin/orders/show

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,6 +6,6 @@ class OrderItem < ApplicationRecord
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :unit_price_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  customizations jsonb default: {}
+  # customizations jsonb default: {}
   store_accessor :customizations
 end
