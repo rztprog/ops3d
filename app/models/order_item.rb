b2acd1084dae24c6fbd99eda44f94199194272dd
rztprog <rztprog@outlook.fr>
2026-05-02 13:34:24 +0200

    Delete jsonb from orderItem model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,6 +6,5 @@ class OrderItem < ApplicationRecord
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :unit_price_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  # customizations jsonb default: {}
   store_accessor :customizations
 end
