2a8bfe3c55c6277ccedd51cd93f8ab2e60acc284
rztprog <rztprog@outlook.fr>
2026-05-07 17:20:29 +0200

    Change cancel_url cart_url to order_url(@order)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -111,7 +111,7 @@ class OrdersController < ApplicationController
       end,
       mode: "payment",
       success_url: order_url(@order),
-      cancel_url: cart_url
+      cancel_url: order_url(@order)
     )
 
     @order.update!(stripe_checkout_session_id: session.id)
