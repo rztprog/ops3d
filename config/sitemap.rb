6c1e240d7c86a22be37c8cb54f22d9320024c8c8
rztprog <rztprog@outlook.fr>
2026-05-11 23:03:07 +0200

    Add fr and en locale to sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -30,15 +30,14 @@ SitemapGenerator::Sitemap.create do
   #     add article_path(article), :lastmod => article.updated_at
   #   end
 
+  %i[fr en].each do |locale|
+    add root_path(locale: locale), changefreq: "daily", priority: locale == :fr ? 1.0 : 0.8
+    add products_path(locale: locale), changefreq: "daily", priority: 0.9
 
-  add root_path(locale: :fr), changefreq: "daily", priority: 1.0
-
-  add products_path(locale: :fr), changefreq: "daily", priority: 0.9
-
-  Product.where(published: true).find_each do |product|
-    add product_path(locale: :fr, id: product),
-        lastmod: product.updated_at,
-        changefreq: "weekly",
-        priority: 0.8
+    Product.where(published: true).find_each do |product|
+      add product_path(locale: locale, id: product),
+          lastmod: product.updated_at,
+          changefreq: "weekly",
+          priority: 0.8
   end
 end
