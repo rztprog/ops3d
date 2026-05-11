a1da9c62150babdf7d73b77a24019c751dd3ee05
rztprog <rztprog@outlook.fr>
2026-05-11 22:17:23 +0200

    Custom sitemap.rb

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = 'http://www.example.com'
+SitemapGenerator::Sitemap.default_host = "https://ops3d.fr"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
@@ -24,4 +24,16 @@ SitemapGenerator::Sitemap.create do
   #   Article.find_each do |article|
   #     add article_path(article), :lastmod => article.updated_at
   #   end
+
+
+  add root_path, changefreq: "daily", priority: 1.0
+
+  add products_path, changefreq: "daily", priority: 0.9
+
+  Product.find_each do |product|
+    add product_path(product),
+        lastmod: product.updated_at,
+        changefreq: "weekly",
+        priority: 0.8
+  end
 end
