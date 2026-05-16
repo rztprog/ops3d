34b59c36a427058f0d103248f3ff1e0b74dde795
rztprog <rztprog@outlook.fr>
2026-05-17 00:29:03 +0200

    Add optional true to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :order_items, dependent: :destroy
 
   STATUSES = %w[pending paid in_preparation shipped cancelled].freeze
