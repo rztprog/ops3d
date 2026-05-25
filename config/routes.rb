8884fb1ee49b620f9001a3982b4650856bf0e8e2
rztprog <rztprog@outlook.fr>
2026-05-25 18:30:03 +0200

    Add guest_orders index

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,8 @@ Rails.application.routes.draw do
     # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
     # Defines the root path route ("/")
+    resources :guest_orders, only: [ :index ]
+
     resources :products, only: [ :index, :show ]
 
     resource :cart, only: [ :show ] do
