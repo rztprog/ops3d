f9324b869bf15bdfaec53bcc73ff6ee80f638dcd
rztprog <rztprog@outlook.fr>
2026-06-02 18:58:11 +0200

    Add column fufillment_mode and stock_quantity to products and db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_20_144210) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_02_165109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -146,9 +146,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_20_144210) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.text "description"
+    t.string "fulfillment_mode", default: "made_to_order", null: false
     t.string "name", null: false
     t.integer "price_cents", null: false
     t.boolean "published", default: false, null: false
+    t.integer "stock_quantity", default: 0, null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
