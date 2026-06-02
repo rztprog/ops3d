fd2c5b720dc157dcccfd72af748d4c43616c5164
rztprog <rztprog@outlook.fr>
2026-06-03 01:24:18 +0200

    Add product_availability_label to products/show and products/index

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -13,7 +13,7 @@ module ProductsHelper
         "bg-green-50 text-green-700 border-green-200" :
         "bg-red-50 text-red-700 border-red-200"
     else
-      "bg-blue-50 text-blue-700 border-blue-200"
+      "bg-white/90 text-gray-700 border-gray-200"
     end
   end
 end
