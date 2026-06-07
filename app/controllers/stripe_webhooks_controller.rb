b36121454974e4067e694cba592a9a6296d024f5
rztprog <rztprog@outlook.fr>
2026-06-07 23:43:22 +0200

    Change Compte Supprimé to Compte invité and change payment_provider: 'stripe (cb)' to 'stripe'

--- app/controllers/stripe_webhooks_controller.rb
+++ app/controllers/stripe_webhooks_controller.rb
@@ -52,7 +52,7 @@ class StripeWebhooksController < ApplicationController
         status: "paid",
         stripe_checkout_session_id: session.id,
         stripe_payment_intent_id: session.payment_intent,
-        payment_provider: "Stripe (CB)"
+        payment_provider: "Stripe"
       )
 
       should_send_emails = true
