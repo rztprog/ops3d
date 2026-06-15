8c7718b7405fed54dcd702095682b03bd831e686
rztprog <rztprog@outlook.fr>
2026-06-15 18:27:04 +0200

    Fix item set to 1 when user add to cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -40,7 +40,13 @@ class CartItemsController < ApplicationController
       end
     else
       cart_item = @cart.cart_items.find_or_initialize_by(product: @product)
-      cart_item.quantity += 1
+
+      if cart_item.new_record?
+        cart_item.quantity = 1
+      else
+        cart_item.quantity += 1
+      end
+
       cart_item.save!
     end
 
