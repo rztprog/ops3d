8beddf177b36bea08ff4be58090e1bb36bd72c98
rztprog <rztprog@outlook.fr>
2026-06-16 22:40:55 +0200

    Fix connected user cannot access to guest_orders page

--- app/controllers/guest_orders_controller.rb
+++ app/controllers/guest_orders_controller.rb
@@ -1,9 +1,20 @@
 class GuestOrdersController < ApplicationController
+  before_action :redirect_authenticated_user
+
   def index
     ids = session[:guest_order_ids] || []
 
     @orders = Order
       .where(id: ids)
       .order(created_at: :desc)
+  end
+
+  private
+
+  def redirect_authenticated_user
+    return unless user_signed_in?
+
+    redirect_to account_orders_path,
+      notice: "Tes commandes sont disponibles ici dans ton compte."
   end
 end
