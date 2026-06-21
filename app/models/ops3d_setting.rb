3f86625a01b8c7c07b6e2bf0e54dcba841b2baa3
rztprog <rztprog@outlook.fr>
2026-06-21 22:55:47 +0200

    Fix brakeman warning XSS LinkToHref

--- app/models/ops3d_setting.rb
+++ app/models/ops3d_setting.rb
@@ -3,6 +3,7 @@ class Ops3dSetting < ApplicationRecord
 
   validates :shipping_mode, presence: true, inclusion: { in: SHIPPING_MODES }
   validates :shipping_price_cents, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validate :announcement_link_url_format
 
   validate :flat_rate_requires_positive_price
 
@@ -17,5 +18,17 @@ class Ops3dSetting < ApplicationRecord
     return if shipping_price_cents.to_i.positive?
 
     errors.add(:shipping_price_cents, "doit être supérieur à 0 pour une livraison forfaitaire")
+  end
+
+  def announcement_link_url_format
+    return if announcement_link_url.blank?
+
+    uri = URI.parse(announcement_link_url)
+
+    return if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+
+    errors.add(:announcement_link_url, "doit être une URL HTTP ou HTTPS valide")
+  rescue URI::InvalidURIError
+    errors.add(:announcement_link_url, "n'est pas valide")
   end
 end
